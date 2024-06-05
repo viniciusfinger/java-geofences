7b84dec53f7847b4b0132a12e74bbd532a1dee46
viniciusfinger <fingervinicius@icloud.com>
2024-06-05 08:56:36 -0300

    Implementing fence exit action

---
+++ src/main/java/br/com/viniciusfinger/geofences/action/FenceExitAction.java
@@ -0,0 +1,52 @@
+package br.com.viniciusfinger.geofences.action;
+
+import br.com.viniciusfinger.geofences.dto.TelemetryDTO;
+import br.com.viniciusfinger.geofences.enums.EventType;
+import br.com.viniciusfinger.geofences.model.Event;
+import br.com.viniciusfinger.geofences.model.Fence;
+import br.com.viniciusfinger.geofences.model.FenceFeatureInterest;
+import br.com.viniciusfinger.geofences.model.Telemetry;
+import br.com.viniciusfinger.geofences.service.EventService;
+import lombok.extern.slf4j.Slf4j;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FenceExitAction implements Action {
+
+    @Autowired
+    private EventService eventService;
+
+    @Override
+    public void execute(FenceFeatureInterest fenceFeatureInterest, TelemetryDTO telemetryDTO, Telemetry lastTelemetry) {
+        Fence fence = fenceFeatureInterest.getFence();
+
+        GeometryFactory geometryFactory = new GeometryFactory();
+
+        Point lastPosition = geometryFactory.createPoint(
+                new Coordinate(lastTelemetry.getLatitude(), lastTelemetry.getLongitude())
+        );
+
+        Point actualPosition = geometryFactory.createPoint(
+                new Coordinate(telemetryDTO.getLatitude(), telemetryDTO.getLongitude())
+        );
+
+        if (isExitingFence(fence, lastPosition, actualPosition)) {
+            log.info("Device {} has entered fence id {}", telemetryDTO.getDeviceId(), fence.getId());
+
+            Event entranceEvent = Event.builder().type(EventType.FENCE_EXIT)
+                    .fenceFeatureInterest(fenceFeatureInterest).build();
+
+            this.eventService.save(entranceEvent);
+            //todo: notification service enviar email
+        }
+    }
+
+    private boolean isExitingFence(Fence fence, Point lastPosition, Point actualPosition) {
+        return fence.getPolygon().contains(lastPosition) && !fence.getPolygon().contains(actualPosition);
+    }
+}
--- src/main/java/br/com/viniciusfinger/geofences/enums/FenceFeature.java
+++ src/main/java/br/com/viniciusfinger/geofences/enums/FenceFeature.java
@@ -1,20 +1,21 @@
 package br.com.viniciusfinger.geofences.enums;
 
 import br.com.viniciusfinger.geofences.action.Action;
+import br.com.viniciusfinger.geofences.action.FenceEntranceAction;
+import br.com.viniciusfinger.geofences.action.FenceExitAction;
 import br.com.viniciusfinger.geofences.action.SpeedLimitAction;
 import lombok.Getter;
 
 @Getter
 public enum FenceFeature {
     SPEED_LIMIT(new SpeedLimitAction()),
-    ENTRANCE_CONTROL(null), //todo: implement entrance control action
-    EXIT_CONTROL(null); //todo: implement exit_control action
+    ENTRANCE_CONTROL(new FenceEntranceAction()),
+    EXIT_CONTROL(new FenceExitAction());
 
     private final Action action;
 
-    private FenceFeature(Action action) {
+    FenceFeature(Action action) {
         this.action = action;
     }
-
 }
 
